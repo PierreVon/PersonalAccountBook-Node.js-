284bda24ea9870bdd46586157adc3ce048a44284
PierreVon <pierrevon@PierreVondeMacBook-Air.local>
2019-04-13 22:25:49 +0800

    create home page

---
+++ controllers/index.js
@@ -0,0 +1,30 @@
+var db = require('../db/db')
+var async = require('async')
+
+// exports.index = (req, res, next) => {
+//     db.query('select * from expenditureTags', (err, results, fields) => {
+//         if(err) { return next(err) }
+//         db.query('select totalMoney from totalMoney', (err2, results2, fields2) => {
+//             if(err2) { return next(err2) }
+//             res.render('io_forms', { title: 'Personal Account Book', totalMoney: results2[0].totalMoney, expenditure_list: results })
+//         })
+//     })
+// }
+
+exports.index = (req, res, next) => {
+    async.parallel({
+        totalMoney: (callback) => {
+            db.query('select totalMoney from totalMoney',(err, result) => {
+                callback(err, result[0].totalMoney)
+            })
+    },
+        expenditure_list: (callback) => {
+            db.query('select * from expenditureTags', (err, result) => {
+                callback(err, result)
+            })
+        }
+    }, (err, results) => {
+        if(err) { return next(err) }
+        res.render('io_forms', { title: 'Personal Account Book', totalMoney: results['totalMoney'], expenditure_list: results['expenditure_list'] })
+})
+}
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+index_controller = require('../controllers/index')
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('io_forms', { title: 'Personal Account Book', totalMoney: 12000 });
+// });
+
+router.get('/', index_controller.index)
+
+module.exports = router;
